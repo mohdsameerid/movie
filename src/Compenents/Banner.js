8a6d323537e232895b382b34383b9a8f8fd50e5b
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-22 13:23:38 +0530

    added getMovies.js in components

--- src/Compenents/Banner.js
+++ src/Compenents/Banner.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import { movies } from './getMovies';
 export default class Banner extends Component {
   render() {
+    let movie = movies.results[0];
     return (
-      <div>
-        
+        <div class="card" >
+        <img class="card-img-top" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">${movie.original_title}</h5>
+          <p class="card-text">${movie.overview}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     )
   }
