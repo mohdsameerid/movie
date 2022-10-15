7e7fe2ac90ba862ea9fed602438be8133ad5907f
mohdsameerid <mohdsameeredu@gmail.com>
2022-10-15 17:53:26 +0530

    some code

--- src/Compenents/Navbar.js
+++ src/Compenents/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div style={{display: "flex", justifyContent:"center", alignItems:"center", backgroundColor:"lightblue", padding:"10px"}}>
+      <div style={{display: "flex", justifyContent:"center", alignItems:"center", backgroundColor:"#001a33", padding:"10px", color:"white"}}>
         <h1 style={{margin:"10px"}}>Movie App</h1>
         <h2 style={{margin:"10px"}}>Favorite</h2>
       </div>
